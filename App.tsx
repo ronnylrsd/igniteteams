c86af820ee82cbccdaff393269248686efbf763f
Ronny Lima Ribeiro da Silva <63603061+ronnylrsd@users.noreply.github.com>
2023-01-25 11:10:48 -0300

    Feat: personalizing fonts

--- App.tsx
+++ App.tsx
@@ -1,13 +1,24 @@
+import {
+  Roboto_400Regular,
+  Roboto_700Bold,
+  useFonts,
+} from "@expo-google-fonts/roboto";
 import { ThemeProvider } from "styled-components";
 
 import theme from "./src/theme";
 
 import { Groups } from "@screens/Groups";
+import { ActivityIndicator } from "react-native";
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    Roboto_400Regular,
+    Roboto_700Bold,
+  });
+
   return (
-    < ThemeProvider theme={theme}>
-      <Groups />
+    <ThemeProvider theme={theme}>
+      {fontsLoaded ? <Groups /> : <ActivityIndicator />}
     </ThemeProvider>
-  )
+  );
 }
