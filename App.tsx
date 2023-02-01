a74989268f18842218d5c0c3818a22f24ba27581
Ronny Lima Ribeiro da Silva <63603061+ronnylrsd@users.noreply.github.com>
2023-02-01 11:02:58 -0300

    Feat: create Players screen

--- App.tsx
+++ App.tsx
@@ -1,16 +1,15 @@
-import { StatusBar } from "react-native";
 import {
   Roboto_400Regular,
   Roboto_700Bold,
   useFonts,
 } from "@expo-google-fonts/roboto";
+import { StatusBar } from "react-native";
 import { ThemeProvider } from "styled-components";
 
 import theme from "./src/theme";
 
 import { Loading } from "@components/Loading";
-import { Groups } from "@screens/Groups";
-import { NewGroup } from "@screens/NewGroup";
+import { Players } from "@screens/Players";
 
 export default function App() {
   const [fontsLoaded] = useFonts({
@@ -25,7 +24,7 @@ export default function App() {
         backgroundColor="transparent"
         translucent
       />
-      {fontsLoaded ? <Groups /> : <Loading />}
+      {fontsLoaded ? <Players /> : <Loading />}
     </ThemeProvider>
   );
 }
