1d8ce17569b2d5a308b1b332671dd8fc71d03fbd
Ronny Lima Ribeiro da Silva <63603061+ronnylrsd@users.noreply.github.com>
2023-02-10 11:20:34 -0300

    Feat: using exceptions

--- src/screens/NewGroup/index.tsx
+++ src/screens/NewGroup/index.tsx
@@ -4,7 +4,9 @@ import { Highlight } from "@components/Highlight";
 import { Input } from "@components/Input";
 import { useNavigation } from "@react-navigation/native";
 import { groupCreate } from "@storage/group/groupCreate";
+import { AppError } from "@utils/AppError";
 import { useState } from "react";
+import { Alert } from "react-native";
 import { Container, Content, Icon } from "./styles";
 
 export function NewGroup() {
@@ -14,10 +16,18 @@ export function NewGroup() {
 
   async function handleNew() {
     try {
+      if(group.trim().length === 0) {
+        return Alert.alert("Novo Grupo", "Informe o nome do grupo");
+      }
       await groupCreate(group);
       navigation.navigate("players", { group });
     } catch (error) {
-      console.log(error);
+      if (error instanceof AppError) {
+        Alert.alert("Novo Grupo", error.message);
+      } else {
+        Alert.alert("Novo Grupo", "Não foi possível criar um novo grupo");
+        console.log(error);
+      }
     }
   }
 
